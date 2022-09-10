9944facb602ae710e96d0b8f6be5232f94d73816
tigoda <101014970+tigoda@users.noreply.github.com>
2022-09-10 22:25:14 +0300

    dz1

---
+++ new_stream/hw1/hw1(2).py
@@ -0,0 +1,5 @@
+# Напишите программу для. проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат
+for x in [0, 1]:
+    for y in [0, 1]:
+        for z in [0, 1]:
+            print(not(x or y or z) == (not x and not y and not z))
